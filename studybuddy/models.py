d4bc26de0b5c1a1bac0a201ceba0266ff1e6295e
uvaakn <akn7hgq@virginia.edu>
2022-04-01 11:58:03 -0400

    Added the Study Session class. Testing first commit

--- studybuddy/models.py
+++ studybuddy/models.py
@@ -4,4 +4,9 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     about = models.CharField(max_length=300); 
-    major = models.CharField(max_length=50); 
+    major = models.CharField(max_length=50);
+
+class StudySession(models.Model):
+    users = models.OneToOneField(User, on_delete=models.CASCADE)
+    time = models.CharField(max_length=10)
+    date = models.CharField(max_length=10)
