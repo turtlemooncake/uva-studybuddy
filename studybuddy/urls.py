8a4994de67a7d8d47b659dd086d5eed2ddfc339e
Mihir Prahlad <mp8dy@virginia.edu>
2022-04-06 17:51:19 -0400

    Added to CSS file, updated navbar

--- studybuddy/urls.py
+++ studybuddy/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
@@ -31,3 +32,4 @@ urlpatterns = [
     path('addCourses/', views.addCourses, name="addCourses"),
     path('logout', views.logOut, name="logout"),
 ]
+urlpatterns += staticfiles_urlpatterns()
