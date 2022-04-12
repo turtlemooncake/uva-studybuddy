89d925847c8f224098862b2d16a4b4f59a67c8d6
uvaakn <akn7hgq@virginia.edu>
2022-04-12 11:35:42 -0400

    missing course
    added missing model

--- studybuddy/models.py
+++ studybuddy/models.py
@@ -5,6 +5,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     about = models.CharField(max_length=300)
     major = models.CharField(max_length=50)
+    courses = models.ManyToManyField('Course', blank=True)
 
 class StudySession(models.Model):
     users = models.ManyToManyField(User)
@@ -12,6 +13,19 @@ class StudySession(models.Model):
     date = models.CharField(max_length=10)
     location = models.CharField(max_length=50)
     subject = models.CharField(max_length=10)
+
+class Course(models.Model):
+    courseAbbv = models.CharField(max_length=5, default='')
+    courseNumber = models.CharField(max_length=5, default='')
+    courseTitle = models.CharField(max_length=120, null=True, default='')
+    courseTopic = models.CharField(max_length=120, null=True, default='')
+
+    def __str__(self):
+        return self.courseAbbv
+
+# class Event(models.Model):
+#     date = models.DateField()
+#     partners = models.ManyToManyField('User', blank=True)
 
 
 
