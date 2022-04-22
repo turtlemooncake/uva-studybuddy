c0b110e645243dc791f7e5f45a4e38a75664736b
turtlemooncake <vickitori109@gmail.com>
2022-04-22 17:02:05 -0400

    chat technincally works but needs brush up

--- studybuddy/views.py
+++ studybuddy/views.py
@@ -12,6 +12,11 @@ from .models import Profile, Course, StudySession, MessageTwo, Room
 from .forms import EditProfileForm, ProfileForm, SessionForm, MessageForm
 from django.contrib.auth import logout
 
+from django.conf import settings
+from django.http import JsonResponse
+
+from twilio.jwt.access_token import AccessToken
+from twilio.jwt.access_token.grants import ChatGrant 
 
 def home(request):
     return render(request, 'home.html')
@@ -229,3 +234,29 @@ def all_rooms(request):
 def room_detail(request, slug):
     room = Room.objects.get(slug=slug)
     return render(request, 'room_detail.html', {'room': room})
+
+def token(request):
+    identity = request.GET.get('identity', request.user.username)
+    device_id = request.GET.get('device', 'default')  # unique device ID
+
+    account_sid = settings.TWILIO_ACCOUNT_SID
+    api_key = settings.TWILIO_API_KEY
+    api_secret = settings.TWILIO_API_SECRET
+    chat_service_sid = settings.TWILIO_CHAT_SERVICE_SID
+
+    token = AccessToken(account_sid, api_key, api_secret, identity=identity)
+
+    # Create a unique endpoint ID for the device
+    endpoint = "MyDjangoChatRoom:{0}:{1}".format(identity, device_id)
+
+    if chat_service_sid:
+        chat_grant = ChatGrant(endpoint_id=endpoint,
+                               service_sid=chat_service_sid)
+        token.add_grant(chat_grant)
+
+    response = {
+        'identity': identity,
+        'token': token.to_jwt()
+    }
+
+    return JsonResponse(response)
